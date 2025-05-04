a031371e02f7132366c596e2a6522b8d072af59a
albertolr98 <alberto.lopezro@alumnos.upm.es>
2025-05-04 19:07:41 +0200

    Cambiado ds4_driver a git submodule

--- tmc4361a_driver/build/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/albertolr98/tfg_ws/src/tmc4361a_driver
-# Build directory: /home/albertolr98/tfg_ws/src/tmc4361a_driver/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("tmc_lib")
