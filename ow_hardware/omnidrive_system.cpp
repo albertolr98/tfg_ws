fa0093b03519182b93342599fce0e91a485cf3b7
albertolr98 <alberto.lopezro@alumnos.upm.es>
2025-11-16 19:25:26 +0100

    Se han limpiado las funciones para contruir el controlador desde 0

--- ow_hardware/omnidrive_system.cpp
+++ ow_hardware/omnidrive_system.cpp
@@ -40,74 +40,13 @@ hardware_interface::CallbackReturn OmnidriveSystemHardware::on_init(
     return hardware_interface::CallbackReturn::ERROR;
   }
 
-  // BEGIN: This part here is for exemplary purposes - Please do not copy to your production code
-  hw_start_sec_ =
-    hardware_interface::stod(info_.hardware_parameters["example_param_hw_start_duration_sec"]);
-  hw_stop_sec_ =
-    hardware_interface::stod(info_.hardware_parameters["example_param_hw_stop_duration_sec"]);
-  // END: This part here is for exemplary purposes - Please do not copy to your production code
+  // TODO(alr): Validate joint interfaces in info_.joints against what the controller expects
+  // (e.g. omni_wheel_drive_controller should expose velocity commands and position/velocity states).
 
-  // Initialize the TMC5160 driver (pins are placeholder values for now)
-  const unsigned int cs_pin = 5;
-  const unsigned int en_pin = 26;
-  TMC5160 driver(cs_pin, en_pin);
-  if (!driver.init())
-  {
-    RCLCPP_FATAL(get_logger(), "Failed to initialize TMC5160 driver.");
-    return hardware_interface::CallbackReturn::ERROR;
-  } 
-  if (!driver.checkComms("OmnidriveSystemHardware"))
-  {
-    RCLCPP_FATAL(get_logger(), "Failed to communicate with TMC5160 driver.");
-    return hardware_interface::CallbackReturn::ERROR;
-  }
+  // TODO(alr): Read driver configuration from info_.hardware_parameters (GPIO pins, wheel radius,
+  // reduction, etc.) and keep them as members so you can configure TMC5160 properly.
 
-  for (const hardware_interface::ComponentInfo & joint : info_.joints)
-  {
-    // DiffBotSystem has exactly two states and one command interface on each joint
-    if (joint.command_interfaces.size() != 1)
-    {
-      RCLCPP_FATAL(
-        get_logger(), "Joint '%s' has %zu command interfaces found. 1 expected.",
-        joint.name.c_str(), joint.command_interfaces.size());
-      return hardware_interface::CallbackReturn::ERROR;
-    }
-
-    if (joint.command_interfaces[0].name != hardware_interface::HW_IF_VELOCITY)
-    {
-      RCLCPP_FATAL(
-        get_logger(), "Joint '%s' have %s command interfaces found. '%s' expected.",
-        joint.name.c_str(), joint.command_interfaces[0].name.c_str(),
-        hardware_interface::HW_IF_VELOCITY);
-      return hardware_interface::CallbackReturn::ERROR;
-    }
-
-    if (joint.state_interfaces.size() != 2)
-    {
-      RCLCPP_FATAL(
-        get_logger(), "Joint '%s' has %zu state interface. 2 expected.", joint.name.c_str(),
-        joint.state_interfaces.size());
-      return hardware_interface::CallbackReturn::ERROR;
-    }
-
-    if (joint.state_interfaces[0].name != hardware_interface::HW_IF_POSITION)
-    {
-      RCLCPP_FATAL(
-        get_logger(), "Joint '%s' have '%s' as first state interface. '%s' expected.",
-        joint.name.c_str(), joint.state_interfaces[0].name.c_str(),
-        hardware_interface::HW_IF_POSITION);
-      return hardware_interface::CallbackReturn::ERROR;
-    }
-
-    if (joint.state_interfaces[1].name != hardware_interface::HW_IF_VELOCITY)
-    {
-      RCLCPP_FATAL(
-        get_logger(), "Joint '%s' have '%s' as second state interface. '%s' expected.",
-        joint.name.c_str(), joint.state_interfaces[1].name.c_str(),
-        hardware_interface::HW_IF_VELOCITY);
-      return hardware_interface::CallbackReturn::ERROR;
-    }
-  }
+  // TODO(alr): Instantiate driver_ with the parsed configuration and perform any hardware checks.
 
   return hardware_interface::CallbackReturn::SUCCESS;
 }
@@ -115,26 +54,8 @@ hardware_interface::CallbackReturn OmnidriveSystemHardware::on_init(
 hardware_interface::CallbackReturn OmnidriveSystemHardware::on_configure(
   const rclcpp_lifecycle::State & /*previous_state*/)
 {
-  // BEGIN: This part here is for exemplary purposes - Please do not copy to your production code
-  RCLCPP_INFO(get_logger(), "Configuring ...please wait...");
-
-  for (int i = 0; i < hw_start_sec_; i++)
-  {
-    rclcpp::sleep_for(std::chrono::seconds(1));
-    RCLCPP_INFO(get_logger(), "%.1f seconds left...", hw_start_sec_ - i);
-  }
-  // END: This part here is for exemplary purposes - Please do not copy to your production code
-
-  // reset values always when configuring hardware
-  for (const auto & [name, descr] : joint_state_interfaces_)
-  {
-    set_state(name, 0.0);
-  }
-  for (const auto & [name, descr] : joint_command_interfaces_)
-  {
-    set_command(name, 0.0);
-  }
-  RCLCPP_INFO(get_logger(), "Successfully configured!");
+  // TODO(alr): Reset state/command interfaces and push configuration to the TMC driver
+  // (current limits, microstepping, ramp parameters, etc.).
 
   return hardware_interface::CallbackReturn::SUCCESS;
 }
@@ -142,23 +63,7 @@ hardware_interface::CallbackReturn OmnidriveSystemHardware::on_configure(
 hardware_interface::CallbackReturn OmnidriveSystemHardware::on_activate(
   const rclcpp_lifecycle::State & /*previous_state*/)
 {
-  // BEGIN: This part here is for exemplary purposes - Please do not copy to your production code
-  RCLCPP_INFO(get_logger(), "Activating ...please wait...");
-
-  for (auto i = 0; i < hw_start_sec_; i++)
-  {
-    rclcpp::sleep_for(std::chrono::seconds(1));
-    RCLCPP_INFO(get_logger(), "%.1f seconds left...", hw_start_sec_ - i);
-  }
-  // END: This part here is for exemplary purposes - Please do not copy to your production code
-
-  // command and state should be equal when starting
-  for (const auto & [name, descr] : joint_command_interfaces_)
-  {
-    set_command(name, get_state(name));
-  }
-
-  RCLCPP_INFO(get_logger(), "Successfully activated!");
+  // TODO(alr): Enable the power stage and make sure state == command to avoid jumps.
 
   return hardware_interface::CallbackReturn::SUCCESS;
 }
@@ -166,45 +71,16 @@ hardware_interface::CallbackReturn OmnidriveSystemHardware::on_activate(
 hardware_interface::CallbackReturn OmnidriveSystemHardware::on_deactivate(
   const rclcpp_lifecycle::State & /*previous_state*/)
 {
-  // BEGIN: This part here is for exemplary purposes - Please do not copy to your production code
-  RCLCPP_INFO(get_logger(), "Deactivating ...please wait...");
-
-  for (auto i = 0; i < hw_stop_sec_; i++)
-  {
-    rclcpp::sleep_for(std::chrono::seconds(1));
-    RCLCPP_INFO(get_logger(), "%.1f seconds left...", hw_stop_sec_ - i);
-  }
-  // END: This part here is for exemplary purposes - Please do not copy to your production code
-
-  RCLCPP_INFO(get_logger(), "Successfully deactivated!");
+  // TODO(alr): Stop the motors, disable driver outputs, and put the hardware in a safe state.
 
   return hardware_interface::CallbackReturn::SUCCESS;
 }
 
 hardware_interface::return_type OmnidriveSystemHardware::read(
-  const rclcpp::Time & /*time*/, const rclcpp::Duration & period)
+  const rclcpp::Time & /*time*/, const rclcpp::Duration & /*period*/)
 {
-  // BEGIN: This part here is for exemplary purposes - Please do not copy to your production code
-  std::stringstream ss;
-  ss << "Reading states:";
-  ss << std::fixed << std::setprecision(2);
-  for (const auto & [name, descr] : joint_state_interfaces_)
-  {
-    if (descr.get_interface_name() == hardware_interface::HW_IF_POSITION)
-    {
-      // Simulate DiffBot wheels's movement as a first-order system
-      // Update the joint status: this is a revolute joint without any limit.
-      // Simply integrates
-      auto velo = get_command(descr.get_prefix_name() + "/" + hardware_interface::HW_IF_VELOCITY);
-      set_state(name, get_state(name) + period.seconds() * velo);
-
-      ss << std::endl
-         << "\t position " << get_state(name) << " and velocity " << velo << " for '" << name
-         << "'!";
-    }
-  }
-  RCLCPP_INFO_THROTTLE(get_logger(), *get_clock(), 500, "%s", ss.str().c_str());
-  // END: This part here is for exemplary purposes - Please do not copy to your production code
+  // TODO(alr): Pull encoder/velocity data from driver_ and update the corresponding state
+  // interfaces with set_state(name, value).
 
   return hardware_interface::return_type::OK;
 }
@@ -212,19 +88,8 @@ hardware_interface::return_type OmnidriveSystemHardware::read(
 hardware_interface::return_type ow_hardware ::OmnidriveSystemHardware::write(
   const rclcpp::Time & /*time*/, const rclcpp::Duration & /*period*/)
 {
-  // BEGIN: This part here is for exemplary purposes - Please do not copy to your production code
-  std::stringstream ss;
-  ss << "Writing commands:";
-  for (const auto & [name, descr] : joint_command_interfaces_)
-  {
-    // Simulate sending commands to the hardware
-    set_state(name, get_command(name));
-
-    ss << std::fixed << std::setprecision(2) << std::endl
-       << "\t" << "command " << get_command(name) << " for '" << name << "'!";
-  }
-  RCLCPP_INFO_THROTTLE(get_logger(), *get_clock(), 500, "%s", ss.str().c_str());
-  // END: This part here is for exemplary purposes - Please do not copy to your production code
+  // TODO(alr): Iterate over joint_command_interfaces_, translate the command to motor setpoints,
+  // and push them to driver_ (speed/position depending on your control mode).
 
   return hardware_interface::return_type::OK;
 }
